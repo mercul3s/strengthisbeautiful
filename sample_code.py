6b778306a3bc221c412e00818eddea28ee2b4702
Susan <onceuponatimeforever@gmail.com>
2012-11-10 10:36:27 -0800

    sample_code.py final verison is now pushed. See instructions at bottom of .py file

--- sample_code.py
+++ sample_code.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+# Outputs a list of strings of the video IDs.
 from apiclient.discovery import build
 from optparse import OptionParser
 
@@ -10,12 +10,13 @@ DEVELOPER_KEY = "AIzaSyBfYR_vMb3llBT-SZqnOUFdLoEwSwi0idQ"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
-def youtube_search(options):
+def youtube_search(options,query):
   youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
     developerKey=DEVELOPER_KEY)
 
   search_response = youtube.search().list(
-    q=options.q,
+    # q=options.q,
+    q=query, 
     part="id,snippet",
     maxResults=options.maxResults
   ).execute()
@@ -23,27 +24,32 @@ def youtube_search(options):
   videos = []
   channels = []
   playlists = []
+  video_idees=[]
 
   for search_result in search_response.get("items", []):
     if search_result["id"]["kind"] == "youtube#video":
       videos.append("%s (%s)" % (search_result["snippet"]["title"],
                                  search_result["id"]["videoId"]))
+      idee=search_result["id"]["videoId"]
+      # print earch_result["id"]["videoId"]
+      video_idees.append(str(idee))
+
     elif search_result["id"]["kind"] == "youtube#channel":
       channels.append("%s (%s)" % (search_result["snippet"]["title"],
                                    search_result["id"]["channelId"]))
     elif search_result["id"]["kind"] == "youtube#playlist":
       playlists.append("%s (%s)" % (search_result["snippet"]["title"],
                                     search_result["id"]["playlistId"]))
-
-  print "Videos:\n", "\n".join(videos), "\n"
-  a="\n".join(videos)
-  print type(a), a
-  return a
+  print video_idees
+  # print "Videos:\n", "\n".join(videos), "\n"
+  video_results="\n".join(videos)
+  # print type(a), a
+  # return video_results
+  return  video_idees
   # print "Videos:\n", "\n".join(videos), "\n"
   # print "Channels:\n", "\n".join(channels), "\n"
   # print "Playlists:\n", "\n".join(playlists), "\n"
 
- 
 if __name__ == "__main__":
   parser = OptionParser()
   parser.add_option("--q", dest="q", help="Search term",
@@ -52,4 +58,6 @@ if __name__ == "__main__":
     help="Max results", default=25)
   (options, args) = parser.parse_args()
 
-  youtube_search(options)
+# Change the keyword search to change the search results. 
+keyword_search="code training women"
+youtube_search(options,  keyword_search)
