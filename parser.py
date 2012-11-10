92773eaddc7ae7c7ccb04ea907b1c315ef13243a
Susan <onceuponatimeforever@gmail.com>
2012-11-10 09:49:04 -0800

    These are newest files. utube API works

---
+++ parser.py
@@ -0,0 +1,290 @@
+# I expect that the Youtube API will output a list of dictionaries, 
+# where the crucially relevant video information is a key called "videoID"
+
+# list_of_dict= [ {"home": "New York City"}, {"zipcode": 11223}, {"id": {"kind":"youtube#video", 
+# 		"videoId":"rdTpA0luD2U"}}]
+list_of_dict={
+ "kind": "youtube#searchListResponse",
+ "etag": "\"J7tN3MQtdAlx5r0eEHriC2eEm_M/xVsXWqCBTQon1mme39NfL94BExk\"",
+ "pageInfo": {
+  "totalResults": 924180,
+  "resultsPerPage": 10
+ },
+ "nextPageToken": "CAoQAA",
+ "items": [
+  {
+   "id": {
+    "kind": "youtube#video",
+    "videoId": "bapO8qCaDyk"
+   },
+   "kind": "youtube#searchResult",
+   "etag": "\"J7tN3MQtdAlx5r0eEHriC2eEm_M/8r-a8B2Mvhf9Fkz0Uz0mVqtYVkw\"",
+   "snippet": {
+    "publishedAt": "2010-02-25T16:04:25.000Z",
+    "channelId": "UC6awBq3lKY35T__puHY2OYQ",
+    "title": "Jesse & Joy - Chocolate",
+    "description": "© 2010 WMG Chocolate ... jesse yjoyoficial ... \"Jesse & Joy\" Chocolate Pop ...",
+    "thumbnails": {
+     "default": {
+      "url": "https://i.ytimg.com/vi/bapO8qCaDyk/default.jpg"
+     },
+     "medium": {
+      "url": "https://i.ytimg.com/vi/bapO8qCaDyk/mqdefault.jpg"
+     },
+     "high": {
+      "url": "https://i.ytimg.com/vi/bapO8qCaDyk/hqdefault.jpg"
+     }
+    }
+   }
+  },
+  {
+   "id": {
+    "kind": "youtube#video",
+    "videoId": "FT62Gwv70kM"
+   },
+   "kind": "youtube#searchResult",
+   "etag": "\"J7tN3MQtdAlx5r0eEHriC2eEm_M/ZJ1azzxDwczulzmL_2rVbR32swY\"",
+   "snippet": {
+    "publishedAt": "2009-06-26T19:02:57.000Z",
+    "channelId": "UCS4bqe_kRDbaKsrpqfHZ08A",
+    "title": "Snow Patrol - Chocolate",
+    "description": "Music video by Snow Patrol performing Chocolate. (C) 2003 Polydor Ltd. (UK) ... Snow Patrol VEVO ... Snow Patrol Polydor ...",
+    "thumbnails": {
+     "default": {
+      "url": "https://i.ytimg.com/vi/FT62Gwv70kM/default.jpg"
+     },
+     "medium": {
+      "url": "https://i.ytimg.com/vi/FT62Gwv70kM/mqdefault.jpg"
+     },
+     "high": {
+      "url": "https://i.ytimg.com/vi/FT62Gwv70kM/hqdefault.jpg"
+     }
+    }
+   }
+  },
+  {
+   "id": {
+    "kind": "youtube#video",
+    "videoId": "0t9xI6EVkuA"
+   },
+   "kind": "youtube#searchResult",
+   "etag": "\"J7tN3MQtdAlx5r0eEHriC2eEm_M/lzB-Ow2XCoku-_Dvk9Jq5Kw6U_A\"",
+   "snippet": {
+    "publishedAt": "2012-07-18T02:53:53.000Z",
+    "channelId": "UCtJ3pVbK_w7xR44TgZZOIzw",
+    "title": "Chocolat",
+    "description": "Chocolate is the beautiful and captivating comedy from the acclaimed director of the Cider House Rules! Nobody could have imagined the impact that ...",
+    "thumbnails": {
+     "default": {
+      "url": "https://i.ytimg.com/vi/0t9xI6EVkuA/default.jpg"
+     },
+     "medium": {
+      "url": "https://i.ytimg.com/vi/0t9xI6EVkuA/mqdefault.jpg"
+     },
+     "high": {
+      "url": "https://i.ytimg.com/vi/0t9xI6EVkuA/hqdefault.jpg"
+     }
+    }
+   }
+  },
+  {
+   "id": {
+    "kind": "youtube#video",
+    "videoId": "rdTpA0luD2U"
+   },
+   "kind": "youtube#searchResult",
+   "etag": "\"J7tN3MQtdAlx5r0eEHriC2eEm_M/yO4d1Q-BxPXFlueCPVbpsQ7J8p4\"",
+   "snippet": {
+    "publishedAt": "2007-05-10T08:52:39.000Z",
+    "channelId": "UC_IHN3yo08JAx1r_kLNpR6Q",
+    "title": "How is chocolate made",
+    "description": "h**p://www.webcastgroup.com/webcast/window_new/asx2.asp?WebcastID=1942&Bandwidth=high ... caloudylou ... chocolate How it's made ...",
+    "thumbnails": {
+     "default": {
+      "url": "https://i.ytimg.com/vi/rdTpA0luD2U/default.jpg"
+     },
+     "medium": {
+      "url": "https://i.ytimg.com/vi/rdTpA0luD2U/mqdefault.jpg"
+     },
+     "high": {
+      "url": "https://i.ytimg.com/vi/rdTpA0luD2U/hqdefault.jpg"
+     }
+    }
+   }
+  },
+  {
+   "id": {
+    "kind": "youtube#video",
+    "videoId": "VoccbDV5NOI"
+   },
+   "kind": "youtube#searchResult",
+   "etag": "\"J7tN3MQtdAlx5r0eEHriC2eEm_M/bUyLCrgbZe_tDVKlu9hZEkiBkt4\"",
+   "snippet": {
+    "publishedAt": "2009-01-20T21:09:43.000Z",
+    "channelId": "UCSCVA33e5Evb-RVIyP1z5rA",
+    "title": "Chocolate Trailer || HD",
+    "description": "Offical Kinotrailer of \"Chocolate\" in HD! Thai director Prachya Pinkaew, who brought us Tony Jaa's incredible and brutal close combat fighting in ...",
+    "thumbnails": {
+     "default": {
+      "url": "https://i.ytimg.com/vi/VoccbDV5NOI/default.jpg"
+     },
+     "medium": {
+      "url": "https://i.ytimg.com/vi/VoccbDV5NOI/mqdefault.jpg"
+     },
+     "high": {
+      "url": "https://i.ytimg.com/vi/VoccbDV5NOI/hqdefault.jpg"
+     }
+    }
+   }
+  },
+  {
+   "id": {
+    "kind": "youtube#video",
+    "videoId": "WjhmB0JlCTo"
+   },
+   "kind": "youtube#searchResult",
+   "etag": "\"J7tN3MQtdAlx5r0eEHriC2eEm_M/FNrTYXBYdL84GwWRja6VON3xPVs\"",
+   "snippet": {
+    "publishedAt": "2010-04-06T10:06:36.000Z",
+    "channelId": "UCoT5KO-FWsYrgu6VW73v3jQ",
+    "title": "Kylie Minogue - Chocolate",
+    "description": "Music video by Kylie Minogue performing Chocolate. ... parlophone ... Kylie Minogue Chocolate EMI UK ...",
+    "thumbnails": {
+     "default": {
+      "url": "https://i.ytimg.com/vi/WjhmB0JlCTo/default.jpg"
+     },
+     "medium": {
+      "url": "https://i.ytimg.com/vi/WjhmB0JlCTo/mqdefault.jpg"
+     },
+     "high": {
+      "url": "https://i.ytimg.com/vi/WjhmB0JlCTo/hqdefault.jpg"
+     }
+    }
+   }
+  },
+  {
+   "id": {
+    "kind": "youtube#video",
+    "videoId": "AqVfuzD_Bcg"
+   },
+   "kind": "youtube#searchResult",
+   "etag": "\"J7tN3MQtdAlx5r0eEHriC2eEm_M/_7o8wyyrxzT03fGzhYOzjY_5WlY\"",
+   "snippet": {
+    "publishedAt": "2012-04-21T09:52:56.000Z",
+    "channelId": "UCaDu0myVZyGibQbk39jbYmA",
+    "title": "Chocolat",
+    "description": "Nobody could have imagined the impact that the striking Vianne (Binoche) would make when she arrived in a tranquil, oldfashioned French town. In ...",
+    "thumbnails": {
+     "default": {
+      "url": "https://i.ytimg.com/vi/AqVfuzD_Bcg/default.jpg"
+     },
+     "medium": {
+      "url": "https://i.ytimg.com/vi/AqVfuzD_Bcg/mqdefault.jpg"
+     },
+     "high": {
+      "url": "https://i.ytimg.com/vi/AqVfuzD_Bcg/hqdefault.jpg"
+     }
+    }
+   }
+  },
+  {
+   "id": {
+    "kind": "youtube#video",
+    "videoId": "Axf_EXzF_qM"
+   },
+   "kind": "youtube#searchResult",
+   "etag": "\"J7tN3MQtdAlx5r0eEHriC2eEm_M/cBzKW_m_A-sDFCtt_BpYlLRNRGk\"",
+   "snippet": {
+    "publishedAt": "2008-01-15T16:21:07.000Z",
+    "channelId": "UCkUFyBZOXwVOT7UJ8NApGew",
+    "title": "Chocolate Trailer (\"ช็อคโกแลต\")",
+    "description": "'Chocolate' stars newcomer Nicharee \"Jeeja\" Vismistananda as an autistic girl who learns how to fight both by absorbing her martial arts skills ...",
+    "thumbnails": {
+     "default": {
+      "url": "https://i.ytimg.com/vi/Axf_EXzF_qM/default.jpg"
+     },
+     "medium": {
+      "url": "https://i.ytimg.com/vi/Axf_EXzF_qM/mqdefault.jpg"
+     },
+     "high": {
+      "url": "https://i.ytimg.com/vi/Axf_EXzF_qM/hqdefault.jpg"
+     }
+    }
+   }
+  },
+  {
+   "id": {
+    "kind": "youtube#video",
+    "videoId": "EwTZ2xpQwpA"
+   },
+   "kind": "youtube#searchResult",
+   "etag": "\"J7tN3MQtdAlx5r0eEHriC2eEm_M/RQDTllfOc-Fe9qb0trTb3EGLc4s\"",
+   "snippet": {
+    "publishedAt": "2007-04-23T06:22:31.000Z",
+    "channelId": "UCjMWn75PwKgutJQ0J3mrLbA",
+    "title": "\"Chocolate Rain\" Original Song by Tay Zonday",
+    "description": "GET THE REMIX!: www.playtay.com ON FACEBOOK! http FREE MP3! Right-click and SAVE! www.tayzonday.net This MP3 is subject to a Creative Commons ...",
+    "thumbnails": {
+     "default": {
+      "url": "https://i.ytimg.com/vi/EwTZ2xpQwpA/default.jpg"
+     },
+     "medium": {
+      "url": "https://i.ytimg.com/vi/EwTZ2xpQwpA/mqdefault.jpg"
+     },
+     "high": {
+      "url": "https://i.ytimg.com/vi/EwTZ2xpQwpA/hqdefault.jpg"
+     }
+    }
+   }
+  },
+  {
+   "id": {
+    "kind": "youtube#video",
+    "videoId": "L5G9a2t8IKI"
+   },
+   "kind": "youtube#searchResult",
+   "etag": "\"J7tN3MQtdAlx5r0eEHriC2eEm_M/qHVmdXuYDCcjWWA2X8XwJmd95Z4\"",
+   "snippet": {
+    "publishedAt": "2008-02-05T05:06:26.000Z",
+    "channelId": "UCVgGMEGRpAwssYLCHNLz4EA",
+    "title": "How To Make Chocolate Lollipops",
+    "description": "www.howdini.com Famous chocolatier Jacques Torres shares his easy recipe for chocolate lollipops. Try this with your kids. You'll all have fun ...",
+    "thumbnails": {
+     "default": {
+      "url": "https://i.ytimg.com/vi/L5G9a2t8IKI/default.jpg"
+     },
+     "medium": {
+      "url": "https://i.ytimg.com/vi/L5G9a2t8IKI/mqdefault.jpg"
+     },
+     "high": {
+      "url": "https://i.ytimg.com/vi/L5G9a2t8IKI/hqdefault.jpg"
+     }
+    }
+   }
+  }
+ ]
+}
+
+# print list_of_dict[0]["c"]  # will produce error message, key not found in index 0 
+# print list_of_dict[0]["home"]
+
+# list_of_dict.get("home")
+# print list_of_dict[2]
+# print range(0,len(list_of_dict))
+
+# print list_of_dict[2].get("zipcode", "zipcode NOT exists!")  
+#if zipcode is not @ index=2, then print not exists!
+
+def get_id(list_of_dict): 
+	for i in range(0,len(list_of_dict)): 
+		a=list_of_dict[i].get("id","id not exist!") 
+		# b=list_of_dict[i].get("home", "home does not exist!")
+	print a
+	return a  #a={"kind":"youtube#video", "videoId":"rdTpA0luD2U"}
+
+def get_videoId(): 
+	a=get_id(list_of_dict)
+	videoID=a.get("videoId", "videoID key does not exist!")
+	print videoID
+
+get_videoId()
---
+++ use_search.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+from apiclient.discovery import build
+from optparse import OptionParser
+import sample_code
+
+
+if __name__ == "__main__":
+	parser = OptionParser()
+	parser.add_option("--q", dest="q", help="Search term",
+	default="Google")
+	parser.add_option("--max-results", dest="maxResults",
+	help="Max results", default=25)
+	(options, args) = parser.parse_args()
+
+
+results= sample_code.youtube_search(options)
+print results, type(results)  # unicode results 
+list_results= list(results)  # listify results  
+print type(list_results), list_results
+
+video_ids= [] 
+for i in list_results: 
+	a= \(.*?\)
+	video_ids.append(a)
+print a
+# for i in results: 
+# 	# print i 
+# 	if ord(i) == 40: 
+# 		start = results.index(i) 
+# 		# print start
