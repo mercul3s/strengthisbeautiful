197c32ca53e9791deced71d2ade04e2dccb5320d
mercul3s <mercul3s@gmail.com>
2012-11-10 10:00:32 -0800

    removed reference to sample_code and commented regex

--- use_search.py
+++ use_search.py
@@ -14,14 +14,14 @@ if __name__ == "__main__":
 	(options, args) = parser.parse_args()
 
 
-results= sample_code.youtube_search(options)
+results= get_videos.youtube_ earch(options)
 print results, type(results)  # unicode results 
 list_results= list(results)  # listify results  
 print type(list_results), list_results
 
 video_ids= [] 
 for i in list_results: 
-	a= \(.*?\)
+	#a= \(.*?\)
 	video_ids.append(a)
 print a
 # for i in results: 
