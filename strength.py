0392f4249fa6d964010279e3be58abbef5e43723
Erin Parker <erinchanparker@gmail.com>
2012-11-10 12:13:09 -0800

    Working front-end

--- strength.py
+++ strength.py
@@ -32,14 +32,14 @@ def display_videos():
 		flash(item)
 	return redirect('/')
 		
-@app.route('/youtube_auth')
-def youtube_auth():
-	return redirect("https://accounts.google.com/o/oauth2/auth?",
-  	client_id=1084945748469-eg34imk572gdhu83gj5p0an9fut6urp5.apps.googleusercontent.com&,
-  	redirect_uri=http://localhost/oauth2callback&,
-  	scope=https://gdata.youtube.com&,
-  	response_type=code&,
-  	access_type=offline)
+# @app.route('/youtube_auth')
+# # def youtube_auth():
+# # 	return redirect("https://accounts.google.com/o/oauth2/auth?",
+# #   	client_id=1084945748469-eg34imk572gdhu83gj5p0an9fut6urp5.apps.googleusercontent.com&,
+# #   	redirect_uri=http://localhost/oauth2callback&,
+# #   	scope=https://gdata.youtube.com&,
+# #   	response_type=code&,
+# #   	access_type=offline)
 
 @app.route('/test_page')
 def test_page():
